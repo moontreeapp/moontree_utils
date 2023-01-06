5203d41b9c3c162874212bf43a02fa248e0392b3
Jordan Miller <Jordan.kay@gmail.com>
2023-01-06 16:25:50 -0700

    added bs58 byte data to string again

--- lib/extensions/bytedata.dart
+++ lib/extensions/bytedata.dart
@@ -2,16 +2,14 @@ import 'dart:typed_data';
 
 import 'package:collection/collection.dart';
 import 'package:convert/convert.dart';
+import 'package:bs58/bs58.dart' as bs58;
 
 extension ByteDataEquality on ByteData {
-  String toHex() {
-    return hex.encode(buffer.asUint8List());
-  }
-
+  String toBs58() => bs58.base58.encode(buffer.asUint8List());
+  String toHex() => hex.encode(buffer.asUint8List());
   // Attempt to copy only relevant portion
-  ByteData clone() {
-    return Uint8List.fromList(buffer.asUint8List()).buffer.asByteData();
-  }
+  ByteData clone() =>
+      Uint8List.fromList(buffer.asUint8List()).buffer.asByteData();
 
   bool equals(Object? other) {
     final thisList = buffer.asUint8List();
