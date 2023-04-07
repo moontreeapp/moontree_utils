9455190b016d34fac08179f4899a2d349e6fab0a
Jordan Miller <Jordan.kay@gmail.com>
2023-04-07 16:31:55 -0600

    added alternative to toSatsCommaString

--- lib/extensions/double.dart
+++ lib/extensions/double.dart
@@ -10,4 +10,28 @@ extension DoubleReadableNumericExtension on double {
       (toString().split('.').last == '0'
           ? ''
           : removeTrailingZeros('.${toStringAsFixed(8).split('.').last}'));
+
+  /// I made this thinking toSatsCommaString did something else. but it is a
+  /// solution for this.
+  String toCommaString({String comma = ','}) {
+    final String fullStr = toString();
+    final List strs = fullStr.split('.');
+    final String str = strs[0];
+    int i = 0;
+    String ret = '';
+    for (final String c in str.characters.reversed) {
+      if (i == 3) {
+        ret = '$c$comma$ret';
+        i = 1;
+      } else {
+        ret = '$c$ret';
+        i += 1;
+      }
+    }
+    if (fullStr.contains('.')) {
+      return '${ret}.${strs.last}';
+    } else {
+      return ret;
+    }
+  }
 }
